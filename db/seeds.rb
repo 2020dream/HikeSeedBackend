87d32d130661579705b79d7d7bd3ac442af305be
Zheng Cao <chori84@gmail.com>
2018-06-26 16:59:19 -0700

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hikes = [
+  {
+    name: "North Creek Trail",
+    lat: "47.796825",
+    lon: "-122.200852",
+    distance: "2.5"
+  },
+  {
+    name: "Sammamish River Trail",
+    lat: "47.758693",
+    lon: "-122.204173",
+    distance: "3.3"
+  },
+  {
+    name: "Big Finn Hill Park",
+    lat: "47.728419",
+    lon: "-122.232826",
+    distance: "1.8"
+  },
+  {
+    name: "Burke-Gilman Trail",
+    lat: "47.758055",
+    lon: "-122.259530",
+    distance: "4.7"
+  }
+]
+
+hikes.each do |hike|
+  Hike.create!(hike)
+end
+
+Seed.create(nickname: 'Wall-E', hike: Hike.all[0])
+Seed.create(nickname: 'Eva', hike: Hike.all[0])
+Seed.create(nickname: 'April', hike: Hike.all[1])
+Seed.create(nickname: 'May', hike: Hike.all[1])
+Seed.create(nickname: 'August', hike: Hike.all[1])
+Seed.create(nickname: 'Sunshine', hike: Hike.all[2])
+Seed.create(nickname: 'Spring', hike: Hike.all[3])
+Seed.create(nickname: 'Summer', hike: Hike.all[3])
+Seed.create(nickname: 'Fall', hike: Hike.all[3])
+Seed.create(nickname: 'Winter', hike: Hike.all[3])
