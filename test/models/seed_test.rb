f0cab279a268c21300a4b30d49a4b6a6bf59bd8b
Zheng Cao <chori84@gmail.com>
2018-06-28 10:15:54 -0700

    Finish model test for Seed

--- test/models/seed_test.rb
+++ test/models/seed_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Seed do
-  let(:seed) { Seed.new }
 
-  it "must be valid" do
-    value(seed).must_be :valid?
+  describe 'validations' do
+    let(:seed) { seeds(:one) }
+
+    it "must be valid" do
+      value(seed).must_be :valid?
+    end
+
+    it "must have nickname" do
+      seed.nickname = nil
+      seed.valid?.must_equal false
+    end
   end
+
+  describe 'relationships' do
+    it "belongs to a hike" do
+      seeds(:one).hike.must_equal hikes(:one)
+    end
+  end
+  
 end
