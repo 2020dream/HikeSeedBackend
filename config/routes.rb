20aefad65c7f0e22c3c80a1874213a71679c977b
Zheng Cao <chori84@gmail.com>
2018-06-28 11:02:49 -0700

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'hikes/index'
-  get 'hikes/show'
-  get 'hikes/create'
-  get 'hikes/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :hikes, only: [:index, :show, :create, :destroy]
+
 end
