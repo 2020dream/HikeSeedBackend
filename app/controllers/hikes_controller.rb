5d0fe2332442a8edc1e7f61aef9adeec99d11a5c
Zheng Cao <chori84@gmail.com>
2018-06-28 11:03:55 -0700

    Finish HikesController test for index

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,5 +1,8 @@
 class HikesController < ApplicationController
   def index
+    @hikes = Hike.all
+
+    render json: @hikes.as_json(only: [:name, :lat, :lon, :distance])
   end
 
   def show
--- test/controllers/hikes_controller_test.rb
+++ test/controllers/hikes_controller_test.rb
@@ -1,24 +1,50 @@
 require "test_helper"
 
 describe HikesController do
-  it "should get index" do
-    get hikes_index_url
-    value(response).must_be :success?
+
+  describe 'index' do
+    it "should get index" do
+      get hikes_url
+
+      expect(response).must_be :successful?
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "should return all hikes with exactly the required fields" do
+      keys = %w(distance lat lon name)
+
+      get hikes_url
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Array
+      expect(body.length).must_equal Hike.count
+      body.each do |hike|
+        expect(hike.keys.sort).must_equal keys
+      end
+    end
+
+    it "should return empty array if there is no hikes" do
+      Hike.all.each do |hike|
+        hike.destroy
+      end
+
+      get hikes_url
+      body = JSON.parse(response.body)
+
+      expect(body.length).must_equal 0
+    end
   end
 
-  it "should get show" do
-    get hikes_show_url
-    value(response).must_be :success?
+  describe 'show' do
+
   end
 
-  it "should get create" do
-    get hikes_create_url
-    value(response).must_be :success?
+  describe 'create' do
+
   end
 
-  it "should get destroy" do
-    get hikes_destroy_url
-    value(response).must_be :success?
+  describe 'destroy' do
+
   end
 
 end
