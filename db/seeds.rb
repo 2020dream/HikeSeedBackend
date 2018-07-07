d8448e3f1ac58e771e5fbcbf2208d598d726f609
Zheng Cao <chori84@gmail.com>
2018-07-06 20:44:41 -0700

    Keep only one hike in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,6 @@ hikes = [
     lat: "47.796825",
     lon: "-122.200852",
     distance: "2.5"
-  },
-  {
-    name: "Sammamish River Trail",
-    lat: "47.758693",
-    lon: "-122.204173",
-    distance: "3.3"
-  },
-  {
-    name: "Big Finn Hill Park",
-    lat: "47.728419",
-    lon: "-122.232826",
-    distance: "1.8"
-  },
-  {
-    name: "Burke-Gilman Trail",
-    lat: "47.758055",
-    lon: "-122.259530",
-    distance: "4.7"
   }
 ]
 
@@ -39,11 +21,3 @@ end
 
 Seed.create(nickname: 'Wall-E', hike: Hike.all[0])
 Seed.create(nickname: 'Eva', hike: Hike.all[0])
-Seed.create(nickname: 'April', hike: Hike.all[1])
-Seed.create(nickname: 'May', hike: Hike.all[1])
-Seed.create(nickname: 'August', hike: Hike.all[1])
-Seed.create(nickname: 'Sunshine', hike: Hike.all[2])
-Seed.create(nickname: 'Spring', hike: Hike.all[3])
-Seed.create(nickname: 'Summer', hike: Hike.all[3])
-Seed.create(nickname: 'Fall', hike: Hike.all[3])
-Seed.create(nickname: 'Winter', hike: Hike.all[3])
