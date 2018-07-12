706bd825d16a6eab91ae1a81e3639bd710cecb50
Zheng Cao <chori84@gmail.com>
2018-07-12 11:17:00 -0700

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,25 @@ hikes = [
     distance: "1.7",
     origin_lat: "47.794123",
     origin_lon: "-122.201123",
-    date: "07-02-2018"
+    date: "06-15-2018"
+  },
+  {
+    name: "Burke Gilman Trail",
+    lat: "47.719802",
+    lon: "-122.280579",
+    distance: "3.2",
+    origin_lat: "47.758003",
+    origin_lon: "-122.263859",
+    date: "06-23-2018"
+  },
+  {
+    name: "Sammamish River Trail",
+    lat: "47.740874",
+    lon: "-122.152917",
+    distance: "1.1",
+    origin_lat: "47.752402",
+    origin_lon: "-122.166221",
+    date: "07-01-2018"
   }
 ]
 
@@ -22,4 +40,8 @@ hikes.each do |hike|
   Hike.create!(hike)
 end
 
-Seed.create(nickname: 'Wall-E', hike: Hike.all[0])
+Seed.create(nickname: 'Vandom', hike: Hike.all[0])
+Seed.create(nickname: 'Micksik', hike: Hike.all[1])
+Seed.create(nickname: 'Nientor', hike: Hike.all[1])
+Seed.create(nickname: 'Ibard', hike: Hike.all[1])
+Seed.create(nickname: 'Anoden', hike: Hike.all[2])
