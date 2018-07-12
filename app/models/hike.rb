925cf0031350ba4d9a9c14687396dbe8e86e3916
Zheng Cao <chori84@gmail.com>
2018-07-12 11:00:10 -0700

    Modify create_with_seeds in Hike.rb

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -8,12 +8,15 @@ class Hike < ApplicationRecord
 
   def self.create_with_seeds(hike_params)
     name = hike_params[:name]
+    origin_lat = hike_params[:origin_lat]
+    origin_lon = hike_params[:origin_lon]
     lat = hike_params[:lat]
     lon = hike_params[:lon]
     distance = hike_params[:distance]
+    date = hike_params[:date]
     seeds = hike_params[:seeds]
 
-    hike = Hike.create(name: name, lat: lat, lon: lon, distance: distance)
+    hike = Hike.create(name: name, origin_lat: origin_lat, origin_lon: origin_lon, lat: lat, lon: lon, distance: distance, date: date)
     if seeds != nil
       seeds.each do |seed|
         Seed.create(nickname: seed[:nickname], hike_id: hike.id)
