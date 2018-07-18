f7ba145b8e19aaf547dc5c0b8b7ce29e6d3c5db8
Zheng Cao <chori84@gmail.com>
2018-07-17 23:17:19 -0700

    Add controller action to update is_harvest

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -2,14 +2,14 @@ class HikesController < ApplicationController
   def index
     @hikes = Hike.all
 
-    render json: @hikes.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date], include: :seeds)
+    render json: @hikes.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, :is_harvest], include: :seeds)
   end
 
   def show
     @hike = Hike.find_by(id: params[:id])
 
     if @hike
-      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date], include: :seeds)
+      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, :is_harvest], include: :seeds)
     else
       render json: {ok: false, :errors => "Hike not found"}, status: :not_found
     end
@@ -19,8 +19,17 @@ class HikesController < ApplicationController
     @hike = Hike.create_with_seeds(hike_params)
 
     if @hike.valid?
-      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date], include: :seeds), status: :ok
+      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, :is_harvest], include: :seeds), status: :ok
     else
+      render json: {ok: false, cause: "validation errors", errors: @hike.errors}, status: :bad_request
+    end
+  end
+
+  def update
+    @hike = Hike.find_by(id: params[:id])
+    @hike.update(is_harvest: true)
+
+    if !@hike.valid?
       render json: {ok: false, cause: "validation errors", errors: @hike.errors}, status: :bad_request
     end
   end
@@ -30,7 +39,7 @@ class HikesController < ApplicationController
 
     if @hike
       @hike.destroy
-      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date]), status: :ok
+      render json: @hike.as_json(only: [:id, :name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, :is_harvest]), status: :ok
     else
       render json: {ok: false, cause: :not_found}, status: :not_found
     end
@@ -39,6 +48,6 @@ class HikesController < ApplicationController
   private
 
   def hike_params
-    return params.permit(:name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, seeds: [:nickname])
+    return params.permit(:name, :origin_lat, :origin_lon, :lat, :lon, :distance, :date, :is_harvest, seeds: [:nickname])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :hikes, only: [:index, :show, :create, :destroy]
+  resources :hikes, only: [:index, :show, :create, :update, :destroy]
   resources :seeds, only: [:destroy]
 
 end
